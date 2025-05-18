eca03b3ab52795d3a48986ee4636fad4dc768b70
Autoz <kuro123520@hotmail.com>
2025-05-18 13:22:39 +0700

    initial

---
+++ ukrit_kosonsomboon.ts
@@ -0,0 +1,10 @@
+function minEnergy (start: number, shops: number[], stations: number[], target: number): number {
+    return 0;
+}
+
+const start: number = 0;
+const shops: number[] = [1, 2, 3, 4, 5];
+const stations: number[] = [1, 2, 3, 4, 5];
+const target: number = 10;
+
+console.log(minEnergy(start, shops, stations, target));
